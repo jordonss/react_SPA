f4dc18de60fd930b3903895d74970e312c5747eb
jordonss <nemezg@gmail.com>
2023-07-27 22:01:27 +0300

    more routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,9 @@ import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
   loader as eventDetailLoader,
+	action as deleteEventAction,
 } from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -33,17 +34,18 @@ const router = createBrowserRouter([
           },
           {
             path: ":eventId",
-						id: 'event-detail',
-						loader: eventDetailLoader,
+            id: "event-detail",
+            loader: eventDetailLoader,
             children: [
               {
                 index: true,
                 element: <EventDetailPage />,
+								action: deleteEventAction,
               },
               { path: "edit", element: <EditEventPage /> },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,25 +1,35 @@
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 
-import EventItem from "../components/EventItem"
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-	const data = useRouteLoaderData('event-detail')
+  const data = useRouteLoaderData("event-detail");
 
-  return (
-    <EventItem event={data.event} />
-  );
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
 
-export async function loader({request, params}) {
-	const id = params.eventId
+export async function loader({ request, params }) {
+  const id = params.eventId;
 
-	const response = await fetch('http://localhost:8080/events/' + id)
+  const response = await fetch("http://localhost:8080/events/" + id);
 
-	if(!response.ok) {
-		throw json({message: 'Could not find selected event'}, {status: 500})
-	} else {
-		return response;
-	}
+  if (!response.ok) {
+    throw json({ message: "Could not find selected event" }, { status: 500 });
+  } else {
+    return response;
+  }
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+		method: request.method,
+	});
+
+  if (!response.ok) {
+    throw json({ message: "Could not delete selected event" }, { status: 500 });
+  }
+  return redirect('/events');
 }
